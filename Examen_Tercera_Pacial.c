42c807707d04c6b32e8730144ea79ce589060e71
JairoPreciado <jairopreciadoayala@gmail.com>
2022-12-05 11:30:40 -0600

    Examen

--- Examen_Tercera_Pacial.c
+++ Examen_Tercera_Pacial.c
@@ -1,42 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
+
 struct nodo{
-       	struct nodo* hijo_izq;
-       	struct nodo* hijo_der;
-       	int info;
+        struct nodo* hijo_izq;
+        struct nodo* hijo_der;
+        int dato;
 }
-struct nodo*i nuevoNodo(int dato){
-	size_t tam_nodo = sizeof(struct nodo);
+
+;struct nodo* nuevoNodo(int dato){
+        size_t tam_nodo = sizeof(struct nodo);
         struct nodo* nuevo_nodo = (struct nodo*) malloc(tam_nodo);
-	nuevo_nodo -> info = dato;
-	nuevo_nodo -> hijo_izq = null;
-	nuevo_nodo -> hijo_der = null;
+        nuevo_nodo->dato = dato;
+        nuevo_nodo->hijo_izq = NULL;
+        nuevo_nodo->hijo_der = NULL;
+
         return nuevo_nodo;
-} 
-void insertarNodo (struct nodo* nd, int dato){
-	if(dato > nd -> dato) {
-		if (nd -> hijo_der == NULL) {
-			nd -> hijo_der = nuevoNodo(dato);
+}
+
+void insertarNodo(struct nodo* nd, int dato){
+        if(dato>nd->dato) {
+                if (nd->hijo_der == NULL) {
+                        nd->hijo_der = nuevoNodo(dato);
                  }else {
-                        insertarNodo(nd -> hijo_der,dato);
+                        insertarNodo(nd->hijo_der,dato);
                  }
          }else {
-		 if (nd -> hijo_izq == NULL) {
-			 nd -> hijo_izq = nuevoNodo(dato);
+                 if (nd->hijo_izq == NULL) {
+                         nd->hijo_izq = nuevoNodo(dato);
                  }else
- 		 {
-                         insertar Nodo(nd -> hijo_izq, dato);
-		 }`~
+                 {
+                         insertarNodo(nd->hijo_izq, dato);
+                 }
          }
-} 
-int busqueda(){
-
 }
-int main ()
- {
+
+
+int nivel = 0;
+
+int buscar(struct nodo* nd, int num){
+        nivel++;
+        if(num == nd->dato){
+                printf("El numero %d se encuentra en el nivel %d\n", num, nivel);
+        }else if(num < nd->dato && nd->hijo_izq != NULL){
+                buscar(nd->hijo_izq, num);
+        }else if(num > nd->dato && nd->hijo_der != NULL){
+                buscar(nd->hijo_der, num);
+        }else{
+                printf("El numero no se encontró\n");
+        }
+        return 0;
+}
+
+int main (){
+int num = 0;
          struct nodo* raiz = NULL;
          raiz = nuevoNodo(8);
          insertarNodo(raiz,3);
          insertarNodo(raiz,20);
+         insertarNodo(raiz,5);
+         insertarNodo(raiz,6);
+         insertarNodo(raiz,9);
+         insertarNodo(raiz,11);
+         insertarNodo(raiz,16);
+
+        printf("Ingresa un numero a encontrar: ");
+        scanf("%d", &num);
+        buscar(raiz, num);
+
  return 0;
 }
+
